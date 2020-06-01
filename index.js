29cfb5d16bc21763e90d67301522a95cf75f64d1
writepro4 <writepro4@naver.com>
2020-06-01 10:35:58 +0900

    [update] index.js 간단한 로그 띄우기 작업

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hi")
+//express란 파일을 express 변수안에 담는작업
+const express = require('express')
+const app = express()
+
+const PORT = 4000;
+
+//콜백 메소드
+const handleListening = () => {
+    console.log(`${PORT}가 열렸어요.`)
+}
+
+//앱 실행할때 할 작업
+app.listen(PORT,handleListening)
