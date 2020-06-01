51fcab71bc4d4d5b7ad64cfc11a77c11be55d009
writepro4 <writepro4@naver.com>
2020-06-01 16:16:40 +0900

    [add] index.js 미들웨어 morgan, helmet, cookie-parser, body-parser 추가

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
 //예전 버전
 // const express = require('express')
@@ -24,11 +28,27 @@ const middleHome = (req, res, next) => {
 //모든 경로 라우터에 미들웨어를 넣고 싶을 경우 app.use를 사용
 //미들웨어는 중간에 로그인체크,log생성,ip주소 체크 등을 하기 위해 사용
 //미들웨어 함수는 아래에 있는 라우터들에만 적용됨. 순서 유의할것
-app.use(middleHome)
+// app.use(middleHome)
+
+//cookie를 사용하기 위한 미들웨어
+app.use(cookieParser())
+//body에 포함된 정보를 얻기 위한 미들웨어
+app.use(bodyParser.urlencoded({extended: true}))
+//json 정보를 얻기 위한 미들웨어
+app.use(bodyParser.json())
+//보안용 미들웨어
+app.use(helmet())
+//로그기록용 미들웨어
+app.use(morgan("tiny"))
+
+//미들웨어는 중간에 끊을 수 있다.
+//next대신 res.send 사용하게 되면 여기서 멈춘다.
+const middleware = (req, res, next) => {
+    res.send("not happening")
+}
 
 //앱 get 주소 url, 실행할 함수
 //맨처음 주소로 들어가면 / 이 경로가 먼저 실행됨.
-//
 app.get("/", handleHome)
 
 app.get("/profile", Profile)
