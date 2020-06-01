05d860c8e6fbbd42980abc846f79673dcd7f92fb
writepro4 <writepro4@naver.com>
2020-06-01 13:51:46 +0900

    [add] .babelrc babel 설치 및 연동작업 / nodemon 설치 및 적용

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-//express란 파일을 express 변수안에 담는작업
-const express = require('express')
+import express from "express";
+
+//예전 버전
+// const express = require('express')
+
 const app = express()
 
-const PORT = 4000;
+const PORT = 4000
 
 //콜백 메소드
-const handleListening = () => {
-    console.log(`${PORT}가 열렸어요.`)
-}
+const handleListening = () => console.log(`${PORT}가 열렸어요.`)
 
-function handleHome(req, res){
+
+const handleHome = (req, res) => {
     console.log("hi home")
     console.log(req)
     res.send(`hellow home`)
 }
 
-function Profile(req, res){
-    res.send("Profile 실행")
-}
+const Profile = (req, res) => res.send("Profile 실행")
+
 
 //앱 get 주소 url, 실행할 함수
 app.get("/", handleHome)
 
-app.get("/profile",Profile)
+app.get("/profile", Profile)
 
 //listen 라우터 생성
-app.listen(PORT,handleListening)
+app.listen(PORT, handleListening)
