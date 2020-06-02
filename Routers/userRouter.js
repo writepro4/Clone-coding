656ac4fa8e2374a65456e30b9107281626bed0b8
writepro4 <writepro4@naver.com>
2020-06-02 12:08:48 +0900

    [update] userRouter.js / videoRouter.js 라우터 경로 추가

--- Routers/userRouter.js
+++ Routers/userRouter.js
@@ -1,9 +1,21 @@
 import express from "express"
+import routes from "./router";
+import {
+    users,
+    userDetail,
+    editProfile,
+    changePassword
+} from "../controller/userController";
 
 const userRouter = express.Router();
 
-// userRouter.get("/",(req, res)=>res.send(`user index`))
-// userRouter.get("/edit",(req, res)=>res.send(`user edit`))
-// userRouter.get("/password",(req, res)=>res.send(`user password`))
+const userRouter = express.Router();
+
+export default userRouter;
+
+userRouter.get(routes.users, users);
+userRouter.get(routes.userDetail, userDetail);
+userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.changePassword, changePassword);
 
 export default userRouter;
--- Routers/videoRouter.js
+++ Routers/videoRouter.js
@@ -1,7 +1,19 @@
-import express from "express"
+import express from "express";
+import routes from "./router";
+import {
+    videos,
+    upload,
+    videoDetail,
+    editVideo,
+    deleteVideo
+} from "../controller/videoController";
 
 const videoRouter = express.Router();
 
+videoRouter.get(routes.videos, videos);
+videoRouter.get(routes.upload, upload);
+videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.editVideo, editVideo);
+videoRouter.get(routes.deleteVideo, deleteVideo);
+
 export default videoRouter;
-
-
